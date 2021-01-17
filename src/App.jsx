d7018ca4a20430be2c9f3df6c7b02dd0b5025ee1
Dilmurod3 <rahimoff.dilmurod@gmail.com>
2021-01-17 14:34:48 +0500

    added new ui

--- src/App.jsx
+++ src/App.jsx
@@ -18,30 +18,49 @@ function App() {
 
   return (
     <div className="App">
-      <input type="search" name="" id="" onChange={handleSetCountry} />
-      <button onClick={handleGetCasesByCountry}>Search</button>
-      {cases ? (
-        cases.data.rows.map((c) => (
-          <>
-            <h2>
-              <b>Country:</b> {c.country}
-            </h2>
-            <h3>
-              <b>Total cases:</b> {c.total_cases}
-            </h3>
-            <h3>
-              <b>Recovered: </b> {c.total_recovered}
-            </h3>
-            <h3>
-              <b>Deaths: </b> {c.total_deaths}
-            </h3>
-            <img src={c.flag} alt="" />
-            <hr />
-          </>
-        ))
-      ) : (
-        <p>No Cases to display!</p>
-      )}
+      <div className="Main">
+        <div className="Tittle-Search">
+          <h1>Covid-19 statistics</h1>
+          <h1>in the world</h1>
+          <input
+            type="search"
+            name=""
+            id=""
+            onChange={handleSetCountry}
+            placeholder="Country"
+          />
+          <button onClick={handleGetCasesByCountry}>Search</button>
+        </div>
+        <div className="Cases-Image">
+          {cases ? (
+            cases.data.rows.map((c) => (
+              <>
+                <div className="Info-Flag">
+                  <div className="Cases">
+                    <h2>
+                      <b>Country:</b> {c.country}
+                    </h2>
+                    <h3>
+                      <b>Total cases:</b> {c.total_cases}
+                    </h3>
+                    <h3>
+                      <b>Recovered: </b> {c.total_recovered}
+                    </h3>
+                    <h3>
+                      <b>Deaths: </b> {c.total_deaths}
+                    </h3>
+                  </div>
+                  <div className="Image">
+                    <img src={c.flag} alt="" />
+                  </div>
+                </div>
+              </>
+            ))
+          ) : (
+            <p>No Cases to display!</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
